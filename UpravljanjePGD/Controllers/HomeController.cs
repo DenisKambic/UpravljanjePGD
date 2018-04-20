6acca04c3fe99ae88932a6e550f159f3b7f60b79
denis kambich <denis.kambic@gmail.com>
2018-04-20 15:54:18 +0200

    naloga

---
+++ UpravljanjePGD/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using UpravljanjePGD.Models;
+
+namespace UpravljanjePGD.Controllers
+{
+	public class HomeController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+		public ActionResult Index()
+		{
+			var posts = db.Posts.OrderByDescending(p => p.Date).Take(5);
+
+			return View(posts.ToList());
+		}
+
+		public ActionResult Zgodovina()
+		{
+			ViewBag.Message = "Kratek opis naše organizacije!";
+
+			return View();
+		}
+
+		public ActionResult Kontakt()
+		{
+			ViewBag.Message = "Naši kontaktni podatki!";
+
+			return View();
+		}
+
+
+		public ActionResult Posts(){
+
+			return View();
+		}
+	}
+}
---
+++ UpravljanjePGD/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UpravljanjePGD.Models
+{
+	public class Post
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(200)]
+		public string Title { get; set; }
+
+		[Required]
+		public string Body { get; set; }
+
+		[Required]
+		public DateTime Date { get; set; }
+
+	}
+}
